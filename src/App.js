823c2e9a86cf57e8c38a8feb1e111e547ba04fc2
sadik5780 <sadik5780@gmail.com>
2023-05-24 16:31:16 +0530

    header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import DashBoard from './Pages/DashBoard';
 import Header from './sharedComponent/Header';
-import "../src/assets/styles/main.scss"
 
 function App() {
   return (
-    <div className="total_body">
-      <Header />
+    <div className="">
+      <Header/>
       <Routes>
         <Route path='/' element={<DashBoard />} />
       </Routes>
