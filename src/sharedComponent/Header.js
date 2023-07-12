df97423a99507558471043701f648815f1c0fafd
Usama7711 <usamamusab16@gmail.com>
2023-07-12 19:12:40 +0530

    changes

--- src/sharedComponent/Header.js
+++ src/sharedComponent/Header.js
@@ -3,24 +3,49 @@ import logo from "../assets/images/Logo.svg"
 import { Link } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <div className='Header_main'>
-        <div className="rightLogo">
-            <img src={logo} alt="" />
-        </div>
-        <div className="Menu">
-                <Link to="#">Home</Link>
-                <Link to="#">About</Link>
-                <Link to="#">What I DO</Link>
-                <Link to="#">Resume</Link>
-                <Link to="#">Portfolio</Link>
-                <Link to="#">Contact</Link>
-        </div>
-        <div className="social_icon">
+  // Active csss
+  window.addEventListener('scroll', function () {
+    var header = document.getElementById('myHeader');
+    var scrollDistance = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollDistance >= window.innerHeight) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
 
-        </div>
-    </div>
+  return (
+
+    // <div className='Header_main'>
+    //   <div className="rightLogo">
+    //     <img src={logo} alt="" />
+    //   </div>
+    //   <div className="Menu">
+    //     <Link to="#">Home</Link>
+    //     <Link to="#">About</Link>
+    //     <Link to="#">What I DO</Link>
+    //     <Link to="#">Resume</Link>
+    //     <Link to="#">Portfolio</Link>
+    //     <Link to="#">Contact</Link>
+    //   </div>
+    //   <div className="social_icon">
+
+    //   </div>
+    // </div>
+    <main>
+      <header id='myHeader'>
+        <nav>
+          <Link to="#">Home</Link>
+          <Link to="#">About</Link>
+          <Link to="#">What I DO</Link>
+          <Link to="#">Resume</Link>
+          <Link to="#">Portfolio</Link>
+          <Link to="#">Contact</Link>
+        </nav>
+      </header>
+    </main>
   )
 }
+
 
 export default Header
