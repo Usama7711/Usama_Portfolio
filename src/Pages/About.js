03a251416faa8bf7e94fcb1653a4d0b00ca7dddd
sadik5780 <sadik5780@gmail.com>
2023-08-16 20:03:54 +0530

    changes

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -45,60 +45,62 @@ const About = () => {
                     </span>
                 </div>
             </div>
-            <div className="headerContain">
-                <div className=" row m-0 px-5 mb-5">
-                    <div className="col-lg-8 px-4">
-                        <div className="ContainHeading">
-                            I'm <span>Usama Musab</span>, a Front End Developer
+            <div className="container">
+                <div className="headerContain">
+                    <div className=" row m-0 mb-5">
+                        <div className="col-lg-8">
+                            <div className="ContainHeading">
+                                I'm <span>Usama Musab</span>, a Front End Developer
+                            </div>
+                            <p className="containDesription">
+                                I help you build brand for your business at an affordable price. Thousands of clients have procured exceptional results while working with our dedicated team. when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                            <p className="containDesription">
+                                Our primary responsibility is to create and implement the user interface and user experience of a website or web application. Our
+                                work involves translating design mockups and wireframes into functional and interactive web pages that users can access and interact with in their browsers
+                            </p>
                         </div>
-                        <p className="containDesription">
-                            I help you build brand for your business at an affordable price. Thousands of clients have procured exceptional results while working with our dedicated team. when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        </p>
-                        <p className="containDesription">
-                            Our primary responsibility is to create and implement the user interface and user experience of a website or web application. Our
-                            work involves translating design mockups and wireframes into functional and interactive web pages that users can access and interact with in their browsers
-                        </p>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="singleLineContain BRbotton">
-                            <p className="Title">Name : </p>
-                            <p className="contain">Usama Musab</p>
+                        <div className="col-lg-4">
+                            <div className="singleLineContain BRbotton">
+                                <p className="Title">Name : </p>
+                                <p className="contain">Usama Musab</p>
+                            </div>
+                            <div className="singleLineContain BRbotton mt-3">
+                                <p className="Title">Email : </p>
+                                <p className="contain"><Link to="#" onClick={handleEmailButtonClick}>usamamusab16@gmail.com</Link></p>
+                            </div>
+                            <div className="singleLineContain BRbotton mt-3">
+                                <p className="Title">Age : </p>
+                                <p className="contain">23</p>
+                            </div>
+                            <div className="singleLineContain mt-3">
+                                <p className="Title">From : </p>
+                                <p className="contain">Nanded, Maharashtra, India</p>
+                            </div>
+                            <label className="DownloadCV">
+                                <a href={IMG} download={cvFileName}>
+                                    <button>Download CV</button>
+                                </a>
+                            </label>
                         </div>
-                        <div className="singleLineContain BRbotton mt-3">
-                            <p className="Title">Email : </p>
-                            <p className="contain"><Link to="#" onClick={handleEmailButtonClick}>usamamusab16@gmail.com</Link></p>
+                    </div>
+                    <div className="WorkRatio mt-4">
+                        <div className="singleRatio">
+                            <div className="countNumber">2.5 +</div>
+                            <div className="ratiotitle">Years Experiance</div>
                         </div>
-                        <div className="singleLineContain BRbotton mt-3">
-                            <p className="Title">Age : </p>
-                            <p className="contain">23</p>
+                        <div className="singleRatio">
+                            <div className="countNumber">12 +</div>
+                            <div className="ratiotitle">Happy Clients</div>
                         </div>
-                        <div className="singleLineContain mt-3">
-                            <p className="Title">From : </p>
-                            <p className="contain">Nanded, Maharashtra, India</p>
+                        <div className="singleRatio">
+                            <div className="countNumber">22 +</div>
+                            <div className="ratiotitle">Project Done</div>
                         </div>
-                        <label className="DownloadCV">
-                            <a href={IMG} download={cvFileName}>
-                                <button>Download CV</button>
-                            </a>
-                        </label>
-                    </div>
-                </div>
-                <div className="WorkRatio px-5 mt-4">
-                    <div className="singleRatio">
-                        <div className="countNumber">2.5 +</div>
-                        <div className="ratiotitle">Years Experiance</div>
-                    </div>
-                    <div className="singleRatio">
-                        <div className="countNumber">12 +</div>
-                        <div className="ratiotitle">Happy Clients</div>
-                    </div>
-                    <div className="singleRatio">
-                        <div className="countNumber">22 +</div>
-                        <div className="ratiotitle">Project Done</div>
-                    </div>
-                    <div className="singleRatio">
-                        <div className="countNumber">{count} +</div>
-                        <div className="ratiotitle">Get Awards</div>
+                        <div className="singleRatio">
+                            <div className="countNumber">{count} +</div>
+                            <div className="ratiotitle">Get Awards</div>
+                        </div>
                     </div>
                 </div>
             </div>
