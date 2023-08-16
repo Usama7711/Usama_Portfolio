9453aa72926064fda68d0d9c7658e25cae6422ea
Usama7711 <usamamusab16@gmail.com>
2023-08-16 18:49:35 +0530

    modal Design

--- src/Pages/Mywork.js
+++ src/Pages/Mywork.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Nav, Tab } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Carousel, Modal, Nav, Tab } from 'react-bootstrap'
 import edsys from "../assets/images/EdsysLogo.svg"
 import Priston from "../assets/images/PristonLogo.svg"
 import VehicleLogo from "../assets/images/VehicleLogo.svg"
 import WifiLogo from "../assets/images/WifiLogo.png"
+import officeLogo from "../assets/images/logo.svg"
+import EdsysPri1 from "../assets/images/EdsysPri1.png"
+import EdsysPri2 from "../assets/images/EdsysPri2.png"
+import EdsysPri3 from "../assets/images/EdsysPri3.png"
 
 
 const Mywork = () => {
+    const [edsysModal, setEdsysModal] = useState(false);
+
+    const handleClose = () => setEdsysModal(false);
+    const handleShow = () => setEdsysModal(true);
     return (
         <div className="MyworkMian">
             <div className="headerMywork">
@@ -43,37 +51,86 @@ const Mywork = () => {
                         <Tab.Content>
                             <Tab.Pane eventKey="first">
                                 <div className="AllProject">
-                                    <div className="singlePro">
-                                        <div className="edsys">
-                                            <img src={edsys} alt="" />
+                                    <div className="row">
+                                        <div className="col-lg-4">
+                                            <div className="singlePro" onClick={() => { handleShow() }}>
+                                                <div className="edsys">
+                                                    <img src={edsys} alt="" />
+                                                </div>
+                                                <div className="backGrondProduct">
+                                                    <p className="projectTitle">Edsys <br /> ( Eduction System & Solutions )</p>
+                                                </div>
+                                            </div>
+                                            <div className="singlePro WifiMain">
+                                                <div className="Wifi">
+                                                    <img src={WifiLogo} alt="" />
+                                                </div>
+                                                <div className="backGrondProduct">
+                                                    <p className="projectTitle">Wifi Attendance <br /> ( Manage Empolyes Data )</p>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="backGrondProduct">
-                                            <p className="projectTitle">Edsys <br /> ( Eduction System & Solutions )</p>
+                                        <div className="col-lg-4">
+                                            <div className="singlePro pristonMain">
+                                                <div className="Priston">
+                                                    <img src={Priston} alt="" />
+                                                </div>
+                                                <div className="backGrondProduct">
+                                                    <p className="projectTitle">Priston <br /> ( Accounting Web Application )</p>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="singlePro pristonMain">
-                                        <div className="Priston">
-                                            <img src={Priston} alt="" />
+                                        <div className="col-lg-4">
+                                            <div className="singlePro VehicleMain">
+                                                <div className="Vehicle">
+                                                    <img src={VehicleLogo} alt="" />
+                                                </div>
+                                                <div className="backGrondProduct">
+                                                    <p className="projectTitle">Vehicle Tracking <br /> ( Live Tracking & Services )</p>
+                                                </div>
+                                            </div>
+                                            <div className="singlePro WifiMain">
+                                                <div className="office">
+                                                    <img src={officeLogo} alt="" />
+                                                </div>
+                                                <div className="backGrondProduct">
+                                                    <p className="projectTitle">Office Caller <br /> ( Live Tracking & Services )</p>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="backGrondProduct">
-                                            <p className="projectTitle">Priston <br /> ( Accounting Web Application )</p>
+
+                                        {/* <div className="singlePro">
+                                            <div className="edsys">
+                                                <img src={edsys} alt="" />
+                                            </div>
+                                            <div className="backGrondProduct">
+                                                <p className="projectTitle">Edsys <br /> ( Eduction System & Solutions )</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="singlePro VehicleMain">
-                                        <div className="Vehicle">
-                                            <img src={VehicleLogo} alt="" />
+                                        <div className="singlePro pristonMain">
+                                            <div className="Priston">
+                                                <img src={Priston} alt="" />
+                                            </div>
+                                            <div className="backGrondProduct">
+                                                <p className="projectTitle">Priston <br /> ( Accounting Web Application )</p>
+                                            </div>
                                         </div>
-                                        <div className="backGrondProduct">
-                                            <p className="projectTitle">Vehicle Tracking <br /> ( Live Tracking & Services )</p>
+                                        <div className="singlePro VehicleMain">
+                                            <div className="Vehicle">
+                                                <img src={VehicleLogo} alt="" />
+                                            </div>
+                                            <div className="backGrondProduct">
+                                                <p className="projectTitle">Vehicle Tracking <br /> ( Live Tracking & Services )</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="singlePro WifiMain">
-                                        <div className="Wifi">
-                                            <img src={WifiLogo} alt="" />
-                                        </div>
-                                        <div className="backGrondProduct">
-                                            <p className="projectTitle">Wifi Attendance <br /> ( Manage Empolyes Data )</p>
-                                        </div>
+                                        <div className="singlePro WifiMain">
+                                            <div className="Wifi">
+                                                <img src={WifiLogo} alt="" />
+                                            </div>
+                                            <div className="backGrondProduct">
+                                                <p className="projectTitle">Wifi Attendance <br /> ( Manage Empolyes Data )</p>
+                                            </div>
+                                        </div> */}
                                     </div>
                                 </div>
                             </Tab.Pane>
@@ -82,6 +139,35 @@ const Mywork = () => {
                     </Tab.Container>
                 </div>
             </div>
+            <Modal show={edsysModal} size='xl' centered onHide={handleClose} className='Common-Modal'>
+                <Modal.Body>
+                    <div className="modal_head">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="heading">Edsys</div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="privewCarosal">
+                                    <Carousel fade>
+                                        <Carousel.Item>
+                                            <img src={EdsysPri1} alt="" />
+                                        </Carousel.Item>
+                                        <Carousel.Item>
+                                            <img src={EdsysPri2} alt="" />
+                                        </Carousel.Item>
+                                        <Carousel.Item>
+                                            <img src={EdsysPri3} alt="" />
+                                        </Carousel.Item>
+                                    </Carousel>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
